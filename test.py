332c36f67eb16ff88a3b7c6b82b5cdbc9b61855f
Chrisitan Solorio <solorich@oregonstate.edu>
2019-07-08 18:23:21 -0700

    Added stemming

--- test.py
+++ test.py
@@ -15,47 +15,49 @@ with open("Reflectivetest1.txt", "r") as myfile:
 contents = str(contents_raw)
 
 tokenized_sent = sent_tokenize(contents)
-
 #print(tokenized_sent)
 
 tokenized_word = word_tokenize(contents)
-
 #print(tokenized_word)
+
+'''
+Filtering of Stop words and punctuation
+'''
+from nltk.corpus import stopwords
+
+stop_words = set(stopwords.words("english")) #Makes a set with the stopwords in the english dictionary
+punctuation = set([".", ",", "''", "''"]) #Makes a set with some punctuation that wasn't being filtered out
+
+stop_words.update(punctuation) #Adds to the set some punctuation that wasn't being filtered out
+
+filtered_contents = [] #Creates an empty list
+
+for w in tokenized_word: #Looks at every word in the text,
+    if w not in stop_words:
+        filtered_contents.append(w)
+'''
+Text stemming
+'''
+from nltk.stem import PorterStemmer
+from nltk.tokenize import sent_tokenize, word_tokenize
+
+ps = PorterStemmer()
+
+stemmed_words = []
+for w in filtered_contents:
+    stemmed_words.append(ps.stem(w))
+
+'''
+Plots most common words found in the texts
+'''
 
 from nltk.probability import FreqDist
 
-'''
-fdist = FreqDist(tokenized_word)
+fdist_stop = FreqDist(filtered_contents) #Creates a list of words with the number of occurences
 
-print(fdist)
+print(fdist_stop) #Prints the number of unique words and total number of words
 
-print(fdist.most_common(5))
-
-import matplotlib.pyplot as plt
-
-fdist.plot(30, cumulative=False)
-
-plt.show
-'''
-
-from nltk.corpus import stopwords
-
-stop_words = set(stopwords.words("english"))
-punctuation = set([".", ",", "''", "''"])
-
-stop_words.update(punctuation)
-
-filtered_contents = []
-
-for w in tokenized_word:
-    if w not in stop_words:
-        filtered_contents.append(w)
-
-fdist_stop = FreqDist(filtered_contents)
-
-print(fdist_stop)
-
-print(fdist_stop.most_common(10))
+print(fdist_stop.most_common(10)) #Prints the 10 most common words
 
 import matplotlib.pyplot as plt
 
